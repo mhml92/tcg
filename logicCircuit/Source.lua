93e76d7b6cd7a3ac1db3783437b314b118935dff
Mikkel Levisen <mikkel.levisen@gmail.com>
2015-07-04 17:37:42 +0200

    added source

---
+++ logicCircuit/Source.lua
@@ -0,0 +1,18 @@
+local Source = Class("Source","Vertex")
+
+function Source:initialize(value)
+	Vertex.initialize(self)
+
+	self.value = value
+end
+
+function Source:value()
+	return self.value
+end
+
+function Source:toggle()
+	self.value = not self.value
+end
+
+return Source
+
